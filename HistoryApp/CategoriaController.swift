347f5c5a3358370cae5f97554f7906fe1118bb4d
Pedro Façanha <pedrofacpeix@gmail.com>
2019-09-16 15:37:17 -0300

    storyboards

---
+++ HistoryApp/CategoriaController.swift
@@ -0,0 +1,30 @@
+//
+//  CategoriaController.swift
+//  HistoryApp
+//
+//  Created by aluno on 16/09/19.
+//  Copyright © 2019 aluno. All rights reserved.
+//
+
+import UIKit
+
+class CategoriaController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HistoryApp/CategoriaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CategoriaViewController.swift
+//  HistoryApp
+//
+//  Created by aluno on 16/09/19.
+//  Copyright © 2019 aluno. All rights reserved.
+//
+
+import UIKit
+
+class CategoriaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
