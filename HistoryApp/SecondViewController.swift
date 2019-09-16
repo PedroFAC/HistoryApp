84b87230ace52ca8b7468d772bb727f72529b54a
Pedro Façanha <pedrofacpeix@gmail.com>
2019-09-16 14:36:11 -0300

    Initial Commit

---
+++ HistoryApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HistoryApp
+//
+//  Created by aluno on 16/09/19.
+//  Copyright © 2019 aluno. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
